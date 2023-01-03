505cb42ad6fb790fa926736fdc0cced3fb866126
gitakshay1 <akshaypatil309p8@gmail.com>
2023-01-03 20:00:30 +0530

    [Akshay]Added program of monthly wage

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -5,29 +5,32 @@
         public const int Is_Full_Time = 1, Is_Part_Time = 2;
         static void Main(string[] args)
         {
-            
-            int Employee_Hour = 0, Wage_perHour = 20, Employee_Wage = 0;
+            int Employee_Hour = 0, Wage_perHour = 20, Daily_Employee_Wage = 0,Monthly_EmpWage=0,Working_Days=20;
             Random Randm= new Random();
-            int empCheck= Randm.Next(0, 3);
-            switch(empCheck)
-            {   
-                case Is_Full_Time:
-                    Console.WriteLine("Employee is Present Full Time");
-                    Employee_Hour = 8;
-                    break;
-                case Is_Part_Time:
-                    Console.WriteLine("Employee is Present Part time");
-                    Employee_Hour = 4;
-                    break;
-                default:
-                    Console.WriteLine("Employee is Absent");
-                    Employee_Hour = 0;
-                    break;
+            for(int day=1;day<=Working_Days; day++)
+            {
+                int empCheck = Randm.Next(0, 3);
+                switch (empCheck)
+                {
+                    case Is_Full_Time:
+                        Console.WriteLine("Employee is Present Full Time");
+                        Employee_Hour = 8;
+                        break;
+                    case Is_Part_Time:
+                        Console.WriteLine("Employee is Present Part time");
+                        Employee_Hour = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Employee is Absent");
+                        Employee_Hour = 0;
+                        break;
 
+                }
+                Daily_Employee_Wage = Employee_Hour * Wage_perHour;
+                Console.WriteLine("Daily Employee Wage is " + Daily_Employee_Wage);
+                Monthly_EmpWage=Monthly_EmpWage+Daily_Employee_Wage;  
             }
-            
-            Employee_Wage = Employee_Hour * Wage_perHour;
-            Console.WriteLine("Employee Wage is " + Employee_Wage);
+            Console.WriteLine("\nMonthly Employee Wage is " + Monthly_EmpWage);
         }
     }
 }
