3e2c024e49e541c965b54fcaea2efed169c83ab3
gitakshay1 <akshaypatil309p8@gmail.com>
2023-01-03 19:44:41 +0530

    [Akshay] Wage using Switch case statement

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -2,26 +2,30 @@
 {
     internal class Program
     {
+        public const int Is_Full_Time = 1, Is_Part_Time = 2;
         static void Main(string[] args)
         {
-            int Is_Full_Time = 1,Is_Part_Time=2, Employee_Hour = 0, Wage_perHour = 20, Employee_Wage = 0;
+            
+            int Employee_Hour = 0, Wage_perHour = 20, Employee_Wage = 0;
             Random Randm= new Random();
             int empCheck= Randm.Next(0, 3);
-            if(empCheck==Is_Full_Time)
-            {
-                Console.WriteLine("Employee is Present Full Time");
-                Employee_Hour = 8;
+            switch(empCheck)
+            {   
+                case Is_Full_Time:
+                    Console.WriteLine("Employee is Present Full Time");
+                    Employee_Hour = 8;
+                    break;
+                case Is_Part_Time:
+                    Console.WriteLine("Employee is Present Part time");
+                    Employee_Hour = 4;
+                    break;
+                default:
+                    Console.WriteLine("Employee is Absent");
+                    Employee_Hour = 0;
+                    break;
+
             }
-            else if(empCheck==Is_Part_Time)
-            {
-                Console.WriteLine("Employee is Present Part time");
-                Employee_Hour = 4;
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-                Employee_Hour = 0;
-            }
+            
             Employee_Wage = Employee_Hour * Wage_perHour;
             Console.WriteLine("Employee Wage is " + Employee_Wage);
         }
