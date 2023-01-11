27339280fd9ddab986a6fb352d199a736c8e3f56
gitakshay1 <akshaypatil309p8@gmail.com>
2023-01-11 21:25:44 +0530

    [Akshay] Added program to save total Employee wage

---
+++ EmployeeWage/EmpWageBuilderObject.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class EmpWageBuilderObject
+    {
+        public const int Is_Full_Time = 1, Is_Part_Time = 2;
+        private string Company;
+        private int Wage_perHour, Working_Days, Max_Hours_InMonth, Monthly_EmpWage;
+        public EmpWageBuilderObject(string Company, int Wage_perHour, int Working_Days, int Max_Hours_InMonth)
+        {
+            this.Company = Company;
+            this.Wage_perHour = Wage_perHour;
+            this.Working_Days = Working_Days;
+            this.Max_Hours_InMonth = Max_Hours_InMonth;
+        }
+        public void ComputionEmpWage()
+        {
+            int Employee_Hour = 0, Monthly_EmpWage = 0,
+                day = 0, Total_Emp_Hour = 0;
+            Random Randm = new Random();
+            while (Total_Emp_Hour < this.Max_Hours_InMonth && day < this.Working_Days)
+            {
+                day++;
+                int empCheck = Randm.Next(0, 3);
+                switch (empCheck)
+                {
+                    case Is_Full_Time:
+                        Console.WriteLine("Employee is Present Full Time");
+                        Employee_Hour = 8;
+                        break;
+                    case Is_Part_Time:
+                        Console.WriteLine("Employee is Present Part time");
+                        Employee_Hour = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Employee is Absent");
+                        Employee_Hour = 0;
+                        break;
+
+                }
+                Total_Emp_Hour +=Employee_Hour;
+                Console.WriteLine("Working Days #" + day + " Employee Hours are " + Employee_Hour);
+            }
+            Monthly_EmpWage = this.Wage_perHour * Total_Emp_Hour;
+            Console.WriteLine("\n Monthly Employee Wage for Company " + Company + " is " + Monthly_EmpWage);
+
+        }
+        public string toString()
+        {
+            return "Total Emp Wage for company : " + this.Company + " is :" + this.Monthly_EmpWage;
+        }
+    }
+}
--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -1,45 +1,17 @@
-﻿namespace Employee_Wage
+﻿using EmployeeWage;
+
+namespace Employee_Wage
 {
     internal class Program
     {
-        public const int Is_Full_Time = 1, Is_Part_Time = 2;
-        public static int ComputionEmpWage(string Company, int Wage_perHour,int Working_Days,int Max_Hours_InMonth)
-        {
-            int Employee_Hour = 0, Daily_Employee_Wage = 0,Monthly_EmpWage=0,
-                day=0,Total_Emp_Hour=0;
-            Random Randm= new Random();
-            while (Total_Emp_Hour < Max_Hours_InMonth && day<Working_Days)
-            {
-                day++;
-                int empCheck = Randm.Next(0, 3);
-                switch (empCheck)
-                {
-                    case Is_Full_Time:
-                        Console.WriteLine("Employee is Present Full Time");
-                        Employee_Hour = 8;
-                        break;
-                    case Is_Part_Time:
-                        Console.WriteLine("Employee is Present Part time");
-                        Employee_Hour = 4;
-                        break;
-                    default:
-                        Console.WriteLine("Employee is Absent");
-                        Employee_Hour = 0;
-                        break;
-
-                }
-                Daily_Employee_Wage = Employee_Hour * Wage_perHour;
-                Total_Emp_Hour = Total_Emp_Hour+ Employee_Hour;
-                Console.WriteLine("Working Days #"+day+" Employee Hours are " + Employee_Hour);
-                Monthly_EmpWage=Monthly_EmpWage+Daily_Employee_Wage;
-            }
-            Console.WriteLine("\n Monthly Employee Wage for Company "+Company+" is " + Monthly_EmpWage);
-            return Monthly_EmpWage;
-        }
         static void Main(string[] args)
         {
-            ComputionEmpWage("Tata",24,22,108);
-            ComputionEmpWage("Mahindra", 22, 26, 110);
+            EmpWageBuilderObject tata = new EmpWageBuilderObject("Tata", 14, 24, 104);
+            EmpWageBuilderObject mahindra = new EmpWageBuilderObject("Mhaindra", 18, 25, 108);
+            tata.ComputionEmpWage();
+            Console.WriteLine(tata.ToString());
+            mahindra.ComputionEmpWage();
+            Console.WriteLine(mahindra.ToString());
         }
     }
 }
