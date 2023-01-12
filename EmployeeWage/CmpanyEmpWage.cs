afad7a9777c19f8c7e6a2a356f43f8aa3fad3fe9
gitakshay1 <akshaypatil309p8@gmail.com>
2023-01-12 23:48:57 +0530

    [Akshay]-Added program for wag of multiple company error is there

---
+++ EmployeeWage/CmpanyEmpWage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class CmpanyEmpWage
+    {
+        private string Company;
+        private int Wage_perHour, Working_Days, Max_Hours_InMonth, Monthly_EmpWage;
+        public CmpanyEmpWage(string company, int wage_perHour, int working_Days, int max_Hours_InMonth)
+        {
+            this.Company = company;
+            this.Wage_perHour = wage_perHour;
+            this.Working_Days = working_Days;
+            this.Max_Hours_InMonth = max_Hours_InMonth;
+           
+        }
+        public void setTotalEmpWage(int Monthly_EmpWage)
+        {
+            this.Monthly_EmpWage= Monthly_EmpWage;
+        }
+        public string toString()
+        {
+            return "Total Emp Wage for company : " + this.Company + " is :" + this.Monthly_EmpWage;
+        }
+    }
+}
--- EmployeeWage/EmpWageBuilderObject.cs
+++ EmployeeWage/EmpWageBuilderObject.cs
@@ -6,24 +6,36 @@ using System.Threading.Tasks;
 
 namespace EmployeeWage
 {
-    internal class EmpWageBuilderObject
+    internal class EmpWageBuilderArray
     {
+        
         public const int Is_Full_Time = 1, Is_Part_Time = 2;
-        private string Company;
-        private int Wage_perHour, Working_Days, Max_Hours_InMonth, Monthly_EmpWage;
-        public EmpWageBuilderObject(string Company, int Wage_perHour, int Working_Days, int Max_Hours_InMonth)
+       
+        private int numOfCompany=0;
+        private CmpanyEmpWage[] cmpanyEmpWagesArray;
+        public EmpWageBuilderArray()
         {
-            this.Company = Company;
-            this.Wage_perHour = Wage_perHour;
-            this.Working_Days = Working_Days;
-            this.Max_Hours_InMonth = Max_Hours_InMonth;
+           this.cmpanyEmpWagesArray = new CmpanyEmpWage[5];
         }
-        public void ComputionEmpWage()
+        public void addCompanyWage(string company, int wage_perHour, int working_Days, int max_Hours_InMonth)
+        {
+            cmpanyEmpWagesArray[this.numOfCompany] = new CmpanyEmpWage(company, wage_perHour, working_Days, max_Hours_InMonth);
+            numOfCompany++;
+        }
+        public void ComputeEmpWage()
+        {
+            for(int i=0; i<numOfCompany; i++)
+            {
+                cmpanyEmpWagesArray[i].setTotalEmpWage(this.ComputeEmpWage(this.cmpanyEmpWagesArray[i]));
+                Console.WriteLine(this.cmpanyEmpWagesArray[i].ToString());
+            }
+        }
+        public int ComputeEmpWage(CmpanyEmpWage cmpanyEmpWage)
         {
             int Employee_Hour = 0, Monthly_EmpWage = 0,
                 day = 0, Total_Emp_Hour = 0;
             Random Randm = new Random();
-            while (Total_Emp_Hour < this.Max_Hours_InMonth && day < this.Working_Days)
+            while (Total_Emp_Hour <=  cmpanyEmpWage.max_Hours_InMonth && day < this.Working_Days)
             {
                 day++;
                 int empCheck = Randm.Next(0, 3);
@@ -50,9 +62,6 @@ namespace EmployeeWage
             Console.WriteLine("\n Monthly Employee Wage for Company " + Company + " is " + Monthly_EmpWage);
 
         }
-        public string toString()
-        {
-            return "Total Emp Wage for company : " + this.Company + " is :" + this.Monthly_EmpWage;
-        }
+        
     }
 }
